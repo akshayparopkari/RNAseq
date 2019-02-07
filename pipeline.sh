95c9cf92d9614e860fbbb927466e32e056309763
akshayparopkari <aparopkari@ucmerced.edu>
2019-02-06 21:47:36 -0800

    Updated version

--- pipeline.sh
+++ pipeline.sh
@@ -28,7 +28,7 @@
 # ---------------------------------------------------------------------------------------
 #+ SCRIPT INFORMATION
 #
-#+ VERSION: 0.0.3
+#+ VERSION: 0.0.4
 #+ AUTHOR:  Akshay Paropkari
 #+ LICENSE: MIT
 #
