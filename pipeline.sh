c0b22feaf29ee16c2bc84fd7fe12033d1d6d82e7
Akshay Paropkari <akshayparopkari@users.noreply.github.com>
2022-08-31 22:10:27 -0700

    Cleaning up the script and conda env. file

--- pipeline.sh
+++ pipeline.sh
@@ -28,7 +28,7 @@
 # ---------------------------------------------------------------------------------------
 #+ SCRIPT INFORMATION
 #
-#+ VERSION: 0.5.0
+#+ VERSION: 0.6.0
 #+ AUTHOR:  Akshay Paropkari
 #+ LICENSE: MIT
 #
@@ -67,10 +67,16 @@ fi
 
 
 # Changing working directory to input directory
-dir=$(realpath "${1}")
-echo -e "\n$(date "+%a %D %r"): Input directory:" "${dir}"  # print the directory name which is being processed
+# print the directory name which is being processed
+echo -e "\n$(date '+%a %D %r') | Input directory identified as ${1}"
+
 # cd into each directory in the directory
-cd "${dir}" || { echo "cd into input directory failed! Please check your working directory." ; exit 1 ; }
+cd "${1}" && echo -e "$(date '+%a %D %r') | Switched into ${1}" || { echo -e "$(date '+%a %D %r') | cd into input directory failed! Please check your working directory."; exit 1; }
+
+# Collect adapter files
+truseq=$(find ~ -type f -name "*truseq.fa.gz" 2>&1 | grep -v "not\ permitted" | tail -n1) && echo -e "$(date '+%a %D %r') | Identified TruSeq Fasta file - ${truseq}"
+polya=$(find ~ -type f -name "*polyA.fa.gz" 2>&1 | grep -v "not\ permitted" | tail -n1) && echo -e "$(date '+%a %D %r') | Located polyA Fasta file - ${polya}"
+genome_dir=$(find ~ -type d -name "ca_genome" 2>&1 | grep -v "not\ permitted" | tail -n1) && echo -e "$(date '+%a %D %r') | Located genome directory - ${genome_dir}"
 
 # Starting preprocessing
 for f in $(find . -type f -name "*.fastq*")
@@ -79,22 +85,20 @@ do
   in_file=$(basename "${f}")
   sample_id=$(basename "${f}" | cut -d_ -f1)
 
-  echo -e "\n$(date "+%a %D %r"): Processing ${sample_id}\n"
+  echo -e "$(date "+%a %D %r") | Processing ${sample_id}"
 
   # •••••••••••••••••••••••••••••••••• #
   # Step 1: Adapter and polyA trimming #
   # •••••••••••••••••••••••••••••••••• #
 
-  echo -e "\n$(date "+%a %D %r"): Adapter and polyA trimming"
+  echo -e "$(date "+%a %D %r") | Adapter and polyA trimming"
 
   # Create output file name and command to run
   trimmed_file="${sample_id}"_trimmed.fastq
-  truseq=$(find /home/aparopkari/.conda/envs/rnaseq -type f -name "*truseq.fa.gz")
-  polya=$(find /home/aparopkari/.conda/envs/rnaseq -type f -name "*polyA.fa.gz")
   CMD1="bbduk.sh in=$in_file out=$trimmed_file ref=$truseq,$polya k=13 ktrim=r mink=5 qtrim=r trimq=20 minlength=20"
 
   # Echo and run command
-  echo "${CMD1}"
+  echo "$(date "+%a %D %r") | ${CMD1}"
   $CMD1
   echo
 
@@ -102,14 +106,14 @@ do
   # Step 2: Fastqc generates a report of sequencing read quality #
   # •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••• #
 
-  echo -e "\n$(date "+%a %D %r"): Read quality assessment"
+  echo -e "$(date "+%a %D %r") | Read quality assessment"
 
   # Create directory to save the quality reports, one per FASTQ file and create command to run
   mkdir -p "${sample_id}"_qc
   CMD2="fastqc -t 24 --nogroup -o ${sample_id}_qc ${trimmed_file}"
 
   # Echo and run command
-  echo "${CMD2}"
+  echo "$(date "+%a %D %r") | ${CMD2}"
   $CMD2
   echo
 
@@ -117,14 +121,13 @@ do
   # Step 3: STAR aligns to  the reference genome and calculates gene counts #
   # ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••• #
 
-  echo -e "\n$(date "+%a %D %r"): Aligning QC reads to Candida albicans A21 genome"
-  genome_dir=$(find ~ -type d -name "ca21_genome_index")
+  echo -e "$(date "+%a %D %r") | Aligning QC reads to Candida albicans A21 genome"
   # Create command to run
   sample_id=$(basename "${f}" .fastq | cut -d_ -f1)
   CMD3="STAR --runThreadN 19 --genomeDir ${genome_dir} --readFilesIn ${trimmed_file} --outFilterType BySJout --outFilterMultimapNmax 25 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.3 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outSAMattributes NH HI NM MD --outSAMtype BAM SortedByCoordinate --quantMode GeneCounts --outFileNamePrefix ${sample_id} > $(basename "${f}" .fastq)_alignment.log"
 
   # Echo and run command
-  echo "${CMD3}"
+  echo "$(date "+%a %D %r") | ${CMD3}"
   $CMD3
   echo
 
@@ -134,14 +137,14 @@ done
 # •••••••••••••••••••••••••••••••• #
 # Collect all counts into one file #
 # •••••••••••••••••••••••••••••••• #
-echo -e "\n$(date "+%a %D %r"): Collecting gene counts for all samples"
+echo -e "$(date "+%a %D %r") | Collecting gene counts for all samples"
 
 # Create command to run
-format_counts=$(find ~ -type f -name "format_counts_table.py" -exec realpath {} \;)
-CMD4="${format_counts} ${dir} -o ${dir}"
+format_counts=$(find ~ -type f -name "format_counts_table.py" 2>&1 | grep -v "not\ permitted" | tail -n1) && echo -e "$(date '+%a %D %r') | Located gene count table formatting script - ${format_counts}"
+CMD4="${format_counts} ${dir} -o ${1}"
 
 # Echo and run command
-echo "${CMD4}"
+echo "$(date "+%a %D %r") | ${CMD4}"
 $CMD4
 echo
 
@@ -152,22 +155,20 @@ echo
 
 # Move log files into a directory
 mkdir -p ./STAR_log ./trim_log
-mv -t STAR_log/ ./*Log.out ./*Log.progress.out ./*Log.final.out ./*_STARtmp/ ./*.out.bam ./*.out.tab
-mv -t trim_log/ *_trimmed.fastq
-
-echo -e "\n$(date "+%a %D %r"): Output files organized"
+mv -t STAR_log/ ./*Log.out ./*Log.progress.out ./*Log.final.out ./*_STARtmp/ ./*.out.bam ./*.out.tab  && echo -e "$(date '+%a %D %r') | Moved STAR intermediate files"
+mv -t trim_log/ *_trimmed.fastq && echo -e "$(date '+%a %D %r') | Moved intermediate adapter trimming files"
 echo
 
 # •••••••••••••••••••••••••••••••••••••••••••••••• #
 # Run differential expression analysis with DESeq2 #
 # •••••••••••••••••••••••••••••••••••••••••••••••• #
-echo -e "\n$(date "+%a %D %r"): Running differential expression analysis using DESeq2"
-METADATA=$(find . -type f -name "*etadata*" -exec realpath {} +)
-GENE_COUNTS=$(find . -type f -name "gene_raw_counts.txt" -exec realpath {} +)
-DESEQ2=$(find ~ -type f -name "deseq.R" -exec realpath {} \;)
+echo -e "$(date "+%a %D %r") | Running differential expression analysis using DESeq2"
+METADATA=$(find . -type f -name "*etadata*" 2>&1 | grep -v "not\ permitted" | tail -n1) && echo -e "$(date '+%a %D %r') | Located metadata file - ${METADATA}"
+GENE_COUNTS=$(find . -type f -name "gene_raw_counts.txt") && echo -e "$(date '+%a %D %r') | Located gene count matrix file - ${GENE_COUNTS}"
+DESEQ2=$(find ~ -type f -name "deseq.R" 2>&1 | grep -v "not\ permitted" | tail -n1) && echo -e "$(date '+%a %D %r') | Located DESEq2 Rscript file - ${DESEQ2}"
 CMD5="${DESEQ2} ${GENE_COUNTS} ${METADATA} ./deseq2_lfc.txt ./MA_plot.pdf"
 
 # Echo and run command
-echo "${CMD5}"
+echo "$(date "+%a %D %r") | ${CMD5}"
 $CMD5
 echo
